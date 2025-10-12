5790ba9f597ac442c4040af80b114c7d6096dbf8
Abrar Ansari <abrartheballer15@gmail.com>
2025-10-12 02:27:12 -0500

    prepating db for training data csv, backup data csv, and corrections csv. Generated a .pkl file to save the ML model

---
+++ intro.sql
@@ -0,0 +1,14 @@
+-- CREATE TABLE training (
+-- id INTEGER PRIMARY KEY,
+-- category TEXT NOT NULL,
+-- name TEXT NOT NULL
+-- );
+
+-- INSERT INTO training (category, name) VALUES
+-- ('strength', 'weightlifting'),
+-- ('strength', 'bodyweight exercises'),
+-- ('cardio', 'running'),
+-- ('cardio', 'cycling'),
+-- ('flexibility', 'yoga'),
+-- ('flexibility', 'stretching') ;
+SELECT * FROM training;
